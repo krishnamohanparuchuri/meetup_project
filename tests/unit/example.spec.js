b2bd4ade5344c1324f6d6f6bb86a2c8086d8d27f
krishnamohanparuchuri <55274056+krishnamohanparuchuri@users.noreply.github.com>
2020-09-17 19:29:50 +0200

    test sample with circle ci

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -9,4 +9,13 @@ describe('HelloWorld.vue', () => {
     })
     expect(wrapper.text()).toMatch(msg)
   })
+
+  it('renders h3 message with "Installed CLI Plugins"', () => {
+    const msg = 'Installed CLI Plugins'
+    const wrapper = shallowMount(HelloWorld, {
+      propsdata: { msg }
+    })
+    const elements_h3 = wrapper.findAll('h3').wrappers
+    expect(elements_h3[0].text()).toBe(msg)
+  })
 })
